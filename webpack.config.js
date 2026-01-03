81aab3e2b7191afefb6f90e54022df5c27af188b
sorokinaolga <sorokinaop@list.ru>
2026-01-03 11:34:42 +0300

    add babel presets

--- webpack.config.js
+++ webpack.config.js
@@ -45,6 +45,16 @@ module.exports = {
           "sass-loader",
         ],
       },
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
     ],
   },
 }
