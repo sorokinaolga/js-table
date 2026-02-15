a6cf66862eddbf7ac2616b0569de4efa30859bfe
sorokinaolga <sorokinaop@list.ru>
2026-02-15 19:41:37 +0300

    подготовлена структура для обработки событий

--- src/components/formula/Formula.js
+++ src/components/formula/Formula.js
@@ -3,9 +3,21 @@ import { ExcelComponent } from "../../core/ExcelComponent";
 export class Formula extends ExcelComponent {
   static className = 'excel__formula';
 
+  constructor($root) {
+    super($root, {
+      name: 'Formula',
+      listeners: ['input', 'click'],
+    });
+  }
 
   toHTML() {
-    return `<div class="info">fx</div>
-      <div class="input" contenteditable spellcheck="false"></div>`;
+    return `
+      <div class="info">fx</div>
+      <div class="input" contenteditable spellcheck="false"></div>
+    `;
+  }
+
+  onInput() {
+    console.log('Formula: onInput', event)
   }
 }
