a631bdae8d8d5a02d6314d61336e67cb876e7ca1
sorokinaolga <sorokinaop@list.ru>
2026-02-15 23:24:07 +0300

    добавлено удаление слушателей событий

--- src/components/toolbar/Toolbar.js
+++ src/components/toolbar/Toolbar.js
@@ -3,6 +3,12 @@ import { ExcelComponent } from "../../core/ExcelComponent";
 export class Toolbar extends ExcelComponent {
   static className = 'excel__toolbar';
    
+  constructor($root) {
+    super($root, {
+      name: 'Toolbar',
+      listeners: ['click'],
+    })
+  }
 
   toHTML() {
     return `<div class="button">
@@ -28,5 +34,9 @@ export class Toolbar extends ExcelComponent {
       <div class="button">
         <i class="material-icons">format_underlined</i>
       </div>`;
+  }
+
+  onClick(event) {
+    console.log(event.target);
   }
 }
--- src/core/DomListener.js
+++ src/core/DomListener.js
@@ -15,13 +15,17 @@ export class DomListener {
       if(!this[method]) {
         throw new Error(`Method ${method} is not implemented in ${this.name || ''} Component`);
       }
+      this[method] = this[method].bind(this);
       // addEventListener
-      this.$root.on(listener, this[method].bind(this));
+      this.$root.on(listener, this[method]);
     })
   }
 
   removeDOMListeners() {
-
+    this.listeners.forEach(listener => {
+      const method = getMethodName(listener);
+      this.$root.off(listener, this[method]);
+    })
   }
 }
 
