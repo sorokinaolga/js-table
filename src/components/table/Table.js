fa3e5efe0f24ac666f0290ab3d43e3cea848bd1c
sorokinaolga <sorokinaop@list.ru>
2026-03-01 13:34:45 +0300

    добавлено изменение размеров строк и столбцов

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,12 +1,23 @@
 import { ExcelComponent } from "../../core/ExcelComponent";
 import { createTable } from "./table.template";
+import { handlerResize } from "./table.resize";
+import { shouldResize } from "./table.functions";
 
 export class Table extends ExcelComponent {
   static className = 'excel__table';
    
-
+  constructor($root) {
+    super($root, {
+      listeners: ['mousedown']
+    })
+  }
   
   toHTML() {
     return createTable();
+  }
+  onMousedown(evt) {
+    if(shouldResize(evt)) {
+      handlerResize(this.$root, evt);
+    }
   }
 }
