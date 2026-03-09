32b818832757b8166ccf6085de886c4f355b2e7d
sorokinaolga <sorokinaop@list.ru>
2026-03-09 08:14:09 +0300

    изменение стилей кнопок тулбара при переходе по ячейкам

--- src/store/initialState.js
+++ src/store/initialState.js
@@ -1,11 +1,13 @@
 import {storage} from '@core/utils'
+import {defaultStyles} from '@core/utils'
 
 const defaultState = {
   rowState: {},
   colState: {},
   dataState: {},
   currentText: '',
-}
+  currentStyles: defaultStyles
+};
 
 export const initialState = storage('table-state')
   ? storage('table-state')
