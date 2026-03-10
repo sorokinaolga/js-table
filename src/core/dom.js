fdfa6144816f931c2c6c0b172d67a0e6de2b7975
sorokinaolga <sorokinaop@list.ru>
2026-03-11 01:13:20 +0300

    добавлена работа формулы

--- src/core/dom.js
+++ src/core/dom.js
@@ -1,115 +1,122 @@
 class Dom {
   constructor(selector) {
-    this.$el = typeof selector === 'string'
-      ? document.querySelector(selector)
-      : selector
+    this.$el =
+      typeof selector === 'string'
+        ? document.querySelector(selector)
+        : selector;
   }
 
   html(html) {
     if (typeof html === 'string') {
-      this.$el.innerHTML = html
-      return this
+      this.$el.innerHTML = html;
+      return this;
     }
-    return this.$el.outerHTML.trim()
+    return this.$el.outerHTML.trim();
   }
 
   text(text) {
-    if (typeof text === 'string') {
-      this.$el.textContent = text
-      return this
+    if (typeof text !== 'undefined') {
+      this.$el.textContent = text;
+      return this;
     }
     if (this.$el.tagName.toLowerCase() === 'input') {
-      return this.$el.value.trim()
+      return this.$el.value.trim();
     }
-    return this.$el.textContent.trim()
+    return this.$el.textContent.trim();
   }
 
   clear() {
-    this.html('')
-    return this
+    this.html('');
+    return this;
   }
 
   on(eventType, callback) {
-    this.$el.addEventListener(eventType, callback)
+    this.$el.addEventListener(eventType, callback);
   }
 
   off(eventType, callback) {
-    this.$el.removeEventListener(eventType, callback)
+    this.$el.removeEventListener(eventType, callback);
   }
 
   find(selector) {
-    return $(this.$el.querySelector(selector))
+    return $(this.$el.querySelector(selector));
   }
 
   append(node) {
     if (node instanceof Dom) {
-      node = node.$el
+      node = node.$el;
     }
 
     if (Element.prototype.append) {
-      this.$el.append(node)
+      this.$el.append(node);
     } else {
-      this.$el.appendChild(node)
+      this.$el.appendChild(node);
     }
 
-    return this
+    return this;
   }
 
   get data() {
-    return this.$el.dataset
+    return this.$el.dataset;
   }
 
   closest(selector) {
-    return $(this.$el.closest(selector))
+    return $(this.$el.closest(selector));
   }
 
   getCoords() {
-    return this.$el.getBoundingClientRect()
+    return this.$el.getBoundingClientRect();
   }
 
   findAll(selector) {
-    return this.$el.querySelectorAll(selector)
+    return this.$el.querySelectorAll(selector);
   }
 
   css(styles = {}) {
-    Object
-        .keys(styles)
-        .forEach(key => {
-          this.$el.style[key] = styles[key]
-        })
+    Object.keys(styles).forEach((key) => {
+      this.$el.style[key] = styles[key];
+    });
   }
 
   getStyles(styles = []) {
     return styles.reduce((res, s) => {
-      res[s] = this.$el.style[s]
-      return res
-    }, {})
+      res[s] = this.$el.style[s];
+      return res;
+    }, {});
   }
 
   id(parse) {
     if (parse) {
-      const parsed = this.id().split(':')
+      const parsed = this.id().split(':');
       return {
         row: +parsed[0],
-        col: +parsed[1]
-      }
+        col: +parsed[1],
+      };
     }
-    return this.data.id
+    return this.data.id;
   }
 
   focus() {
-    this.$el.focus()
-    return this
+    this.$el.focus();
+    return this;
+  }
+
+  attr(name, value) {
+    if (value) {
+      this.$el.setAttribute(name, value);
+      return this;
+    }
+    return this.$el.getAttribute(name);
   }
 
   addClass(className) {
-    this.$el.classList.add(className)
-    return this
+    this.$el.classList.add(className);
+    return this;
   }
 
   removeClass(className) {
-    this.$el.classList.remove(className)
-    return this
+    this.$el.classList.remove(className);
+    return this;
   }
 }
 
